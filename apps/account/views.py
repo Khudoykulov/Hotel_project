1512bd8fe797f472871a87ac87e0d698ee0afa9c
Khudoykulov <155818140+Khudkulov@users.noreply.github.com>
2024-02-11 23:17:21 +0500

    4

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, TemplateView
+from .form import UserCreationForm, MyUserCreationForm
 
 
-class RegisterView(TemplateView):
+class RegisterView(CreateView):
+    form_class = MyUserCreationForm
+
     template_name = 'account/register.html'
 
 
