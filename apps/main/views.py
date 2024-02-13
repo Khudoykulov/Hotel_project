9b7bd900690c730f8f6ff91d0c0df019862ab7a3
Khudoykulov <155818140+Khudkulov@users.noreply.github.com>
2024-02-13 17:25:11 +0500

    14

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,10 +3,16 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 from .models import Contact
 from apps.main.form import ContactForm
+from apps.blog.models import BlogPost
 
 
 class HomePageView(TemplateView):
     template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        cnt = super().get_context_data(**kwargs)
+        cnt['blog_list'] = BlogPost.objects.order_by('-id')[:3]
+        return cnt
 
 
 class ContactView(CreateView):
