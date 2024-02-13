2034683425a5122857f9a36dd9a6fb92ff59bb4a
Khudoykulov <155818140+Khudkulov@users.noreply.github.com>
2024-02-13 16:58:51 +0500

    13

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import BlogPost, Tag
+from .models import BlogPost, Tag, Content
 from django.views.generic import TemplateView, CreateView, ListView
 
 
@@ -10,14 +10,19 @@ class BlogListView(ListView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['object_3'] = BlogPost.objects.all()[:3]
+        ctx['object_3'] = BlogPost.objects.order_by('-id')[:3]
         ctx['tags'] = Tag.objects.all()
         return ctx
 
 
 def blogdetail(request, slug):
+    blogs = BlogPost.objects.order_by('-id')[:3]
     blog = get_object_or_404(BlogPost, slug=slug)
+    tags = Tag.objects.all()
     context = {
-        'blog': blog
+        'blog': blog,
+        'blogs': blogs,
+        'tags': tags,
+
     }
     return render(request, 'blog/single-blog.html', context)
