a7a88db785078e0862083fc26dc396b40115a808
Khudoykulov <155818140+Khudkulov@users.noreply.github.com>
2024-02-13 15:34:52 +0500

    12

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from .views import (
+    BlogListView,
+    blogdetail
+)
+
 app_name = 'blog'
 
 urlpatterns = [
-
+    path('blog_list', BlogListView.as_view(), name='blog_list'),
+    path('blog_detail/<slug:slug>', blogdetail, name='blog_detail')
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost, Tag
+from django.views.generic import TemplateView, CreateView, ListView
+
+
+class BlogListView(ListView):
+    queryset = BlogPost.objects.all()
+    template_name = 'blog/blog.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['object_3'] = BlogPost.objects.all()[:3]
+        ctx['tags'] = Tag.objects.all()
+        return ctx
+
+
+def blogdetail(request, slug):
+    blog = get_object_or_404(BlogPost, slug=slug)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/single-blog.html', context)
